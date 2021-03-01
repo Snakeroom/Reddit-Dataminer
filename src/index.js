bcfe7b12d4eda5397e4c008a3d4645c2201eace5
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-03-01 18:49:49 -0500

    Update Caporal to version 2.0.2

--- src/index.js
+++ src/index.js
@@ -1,27 +1,53 @@
-const cli = require("caporal");
+const { program } = require("@caporal/core");
 const { debug } = require("./util/log.js");
 
 const start = require("./start.js");
 
 const { version } = require("../package.json");
-cli.version(version);
+program.version(version);
 
-const debugOpt = ["--debug [debug]", "Debuggers to enable.", cli.STRING, "reddit-dataminer:*"];
+/**
+ * @type {[string, string, import('@caporal/core').CreateOptionCommandOpts]}
+ */
+const debugOpt = ["--debug [debug]", "Debuggers to enable.", {
+	default: "reddit-dataminer:*",
+	validator: program.STRING,
+}];
 
-cli
+program
 	.command("start", "Starts the script dumper.")
 	.option(...debugOpt)
-	.option("--path [path]", "The output path.", cli.STRING, null, true)
-	.option("--banner [banner]", "The banner comment supporting placeholders", cli.ARRAY, [
-		"{URL}",
-		"Retrieved at {DATE} by Reddit Dataminer v{VERSION}",
-	])
-	.option("--hashes [hashes]", "A path to hashes, to prevent archiving a file if its hash is the same.", cli.STRING)
-	.option("--no-sandbox", "Whether the Puppeteer sandbox should be enabled.", cli.BOOLEAN)
-	.option("--cache", "Whether the Puppeteer cache should be enabled.", cli.BOOLEAN, false)
-	.option("--map-index [map-index]", "The index of the object mapping file names to hashes in the runtime script.", cli.INTEGER, 1)
-	.option("--reddit-username [reddit-username]", "The Reddit user's username.", cli.STRING)
-	.option("--reddit-password [reddit-password]", "The Reddit user's password.", cli.STRING)
+	.option("--path [path]", "The output path.", {
+		required: true,
+		validator: program.STRING,
+	})
+	.option("--banner [banner]", "The banner comment supporting placeholders", {
+		default: [
+			"{URL}",
+			"Retrieved at {DATE} by Reddit Dataminer v{VERSION}",
+		],
+		validator: program.ARRAY,
+	})
+	.option("--hashes [hashes]", "A path to hashes, to prevent archiving a file if its hash is the same.", {
+		validator: program.STRING,
+	})
+	.option("--no-sandbox", "Whether the Puppeteer sandbox should be enabled.", {
+		validator: program.BOOLEAN,
+	})
+	.option("--cache", "Whether the Puppeteer cache should be enabled.", {
+		default: false,
+		validator: program.BOOLEAN,
+	})
+	.option("--map-index [map-index]", "The index of the object mapping file names to hashes in the runtime script.", {
+		default: 1,
+		validator: program.NUMBER,
+	})
+	.option("--reddit-username [reddit-username]", "The Reddit user's username.", {
+		validator: program.STRING,
+	})
+	.option("--reddit-password [reddit-password]", "The Reddit user's password.", {
+		validator: program.STRING,
+	})
 	.action((arguments2, options) => {
 		const args = Object.assign(arguments2, options);
 		debug.enable(args.debug);
@@ -29,4 +55,4 @@ cli
 		start(args);
 	});
 
-cli.parse(process.argv);
+program.run(process.argv.slice(2));
